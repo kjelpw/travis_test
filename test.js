40891c336d9999045f43839c387811eb69374de2
kjepette <kjel.pettenger-willey@du.edu>
2020-05-08 08:44:10 -0700

    hello world

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
-function testFunction(a, b) {
-  return a > b;
-}
+var express = require('express');
+var app = express();app.get('/', function (req, res) {
+  res.send('Hello World!');
+});app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
