6d9b00199e50935c439306e94aed841e08c06585
kjepette <kjel.pettenger-willey@du.edu>
2020-05-21 09:00:50 -0700

    inclusion of tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,25 @@
-var gulp = require('gulp'),
-    uglify = require('gulp-uglify-es').default,
-    plumber = require('gulp-plumber');
+const gulp = require('gulp'),
+      uglify = require('gulp-uglify-es').default,
+      javascriptObfuscator = require('gulp-javascript-obfuscator');
 
-gulp.task('default', function () {
-    return 0;
-});
+var minify_js = function() {
+  return gulp.src('./public/assets/js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/dist/assets/js/'));
+}
+exports.minify_js = minify_js;
 
-gulp.task('dependancies', function () {
-    return gulp.src('./public/assets/js/*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest('./public/dist/assets/js/'));
-});
+var obfuscate_js = function() {
+  return gulp.src('./public/assets/js/*.js')
+    .pipe(javascriptObfuscator())
+    .pipe(gulp.dest('./public/dist/assets/js/'));
+}
+exports.obfuscate_js = obfuscate_js;
 
-gulp.task("libs", function () {
-    return gulp.src("./libs/*.js")
-        .pipe(uglify(/* options */))
-        // .pipe(rename("bundle.min.js"))
-        .pipe(gulp.dest("./public/dist/libs/"));
-});
+var deploy_js = function() {
+  return gulp.src('./public/dist/assets/js/*.js')
+    .pipe(gulp.dest('./public/assets/js/'));
+}
+exports.deploy_js = deploy_js;
+
+gulp.task('deploy_all', gulp.series(obfuscate_js, deploy_js))
