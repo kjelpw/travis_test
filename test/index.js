a3b88d5cb1f4985e7db29076b5613d76b4391c56
kjelpw <kjelpw@protonmail.com>
2020-07-08 12:37:44 -0700

    console output moved to after expect

--- test/index.js
+++ test/index.js
@@ -217,8 +217,8 @@ describe('Special Collections (Selenium) Tests', function() {
           return browser.findElement(webdriver.By.xpath('//*[@id="collections-window"]'))
           .isDisplayed()
           .then(function(visible) {
-            console.log('HERE');
             expect(visible).to.equal(false);
+            console.log('HERE');
           });
         });
 
